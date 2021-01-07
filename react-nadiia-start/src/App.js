90a94d0170e58046f2d535b35bc9a64533b6f43a
Nadiia <npodufala15@gmail.com>
2021-01-07 16:29:17 +0200

    start

---
+++ react-nadiia-start/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App =() => {
+  return (
+    <div className="App">
+     <ul>
+       <li>REACT</li>
+       <li>HTML</li>
+       <li>SCC</li>
+       <li>JS</li>
+
+     </ul>
+    </div>
+  );
+}
+
+export default App;
