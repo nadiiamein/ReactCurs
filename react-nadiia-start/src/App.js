7bef39db771288a1bec9a4dccf43fb1617f8c019
Nadiia <npodufala15@gmail.com>
2021-01-07 22:30:32 +0200

    newkomponent

--- react-nadiia-start/src/App.js
+++ react-nadiia-start/src/App.js
@@ -2,16 +2,14 @@ import './App.css';
 
 const App =() => {
   return (
-    <div className="App">
-     <ul>
-       <li>REACT</li>
-       <li>HTML</li>
-       <li>SCC</li>
-       <li>JS</li>
-
-     </ul>
+    <div>
+    <Header />
+    <Technologies/>
+    
     </div>
   );
 }
+
+
 
 export default App;
---
+++ react-nadiia-start/src/Technologies.js
@@ -0,0 +1,16 @@
+
+const Technologies = () => {
+    return (
+  <div >
+       <ul>
+         <li>REACT</li>
+         <li>HTML</li>
+         <li>SCC</li>
+         <li>JS</li>
+  
+       </ul>
+      </div>
+    )
+  }
+
+  export default Technologies;
