7123c3315f041e21623a9286785623d1ccb062ca
Nadiia <npodufala15@gmail.com>
2021-01-13 15:43:29 +0200

    message

---
+++ react-nadiia-start/src/componets/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './../Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
+
+const DialogItem = (props) => {
+    let path = "dialogs/1" + props.id;
+    return (<div className={s.dialog + '' + s.active}>
+<NavLink to={path}>{props.name}</NavLink>
+    </div>
+    );
+}
+
+
+export default DialogItem; 
