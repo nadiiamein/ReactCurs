c29b7cd7368f5f038df4d80b536340a47af648ed
Nadiia <npodufala15@gmail.com>
2021-01-17 16:29:10 +0200

    addpost

--- react-nadiia-start/src/componets/Profile/MyPosts/MyPosts.jsx
+++ react-nadiia-start/src/componets/Profile/MyPosts/MyPosts.jsx
@@ -10,9 +10,9 @@ let postsElements = props.posts.map( p => <Post message={p.message} likesCount= 
 let newPostElement = React.createRef();
 
   let addPost = () => {
-    debugger;
     let text = newPostElement.current.value;
     props.addPost(text);
+    newPostElement.current.value = '';
   }
 
     return (
