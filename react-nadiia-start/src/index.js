90e4dbd2f59a877b50fcc44129df4067feb0e05b
nadiiamein <npodufala15@gmail.com>
2021-12-22 17:19:54 +0100

    add servisevorker and old-store

--- react-nadiia-start/src/index.js
+++ react-nadiia-start/src/index.js
@@ -1,25 +1,11 @@
-import * as serviceWorker from './serviceWorker';
-import App from './App';
-import store from './redux/redux-store.js';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
+import App from "./Components/App/App";
 
-
- 
-    ReactDOM.render (
-      <BrowserRouter>
-      <Provider store={store}>
-        <App /> 
-        </Provider>
-  </BrowserRouter>, document.getElementById('root'));
- 
-
+ReactDOM.render(<App />, document.getElementById("root"));
 //renderEntireTree();
-
 
 // store.subscribe(() => {
 //   renderEntireTree();
